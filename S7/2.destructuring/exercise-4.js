26ad33a0af2ef0b7128df3d38b7c59f9db99ef04
fpascuales <fpascuales@gmail.com>
2023-02-05 18:32:25 +0100

    Terminados los ejercicios de destructuring

---
+++ S7/2.destructuring/exercise-4.js
@@ -0,0 +1,8 @@
+const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
+const {name, itv} = car
+const [y1, y2, y3] = itv
+console.log(name)
+console.log(itv)
+console.log(y1)
+console.log(y2)
+console.log(y3)
