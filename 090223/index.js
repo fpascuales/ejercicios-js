8d274d044477d6cde6666b52d2d9f5ede1521ecc
fpascuales <fpascuales@gmail.com>
2023-02-10 22:48:08 +0100

    Pokedex terminada a falta de responsive

--- 090223/index.js
+++ 090223/index.js
@@ -1,10 +1,94 @@
-//try catch no está hecho para atender errores sintácticos, sino para errores de ejecución
-console.log("primera linea");
-try{
-    //código susceptible a fallar
-
-}catch(err){
-    //código que se ejecutará cuando falle el código del try
-    console.log("linea catch");
+const divLogs = document.getElementById('div-logs')
+const input = document.getElementById('input')
+const db = {
+    logs: [
+        {id: 1, text: 'Error en base de datos', date:'22:47'},
+        {id: 2, text: 'Error en formulario', date:'22:48'},
+        {id: 3, text: 'Error en impresora', date:'22:49'},
+    ]
 }
-console.log("segunda linea");
+let temporizadorBusqueda = null
+input.addEventListener('keyup', ()=>{
+    //si el tempo está activo
+    if(temporizadorBusqueda){
+        //lo paro
+        clearTimeout(temporizadorBusqueda)
+    }
+    temporizadorBusqueda = setTimeout(() => {
+        console.log("estoy filtrando...");
+        printarLogs(db.logs.filter(x=> x.text.toLowerCase().includes(input.value.toLowerCase())))
+    },0.5*1000)
+})
+
+const printarLogs = (logFiltrados) => {
+    divLogs.innerHTML = ''
+    const tabla = document.createElement("table")
+    tabla.className = "tabla-logs"
+    const tableHead = document.createElement("thead")
+    const trHead = document.createElement("tr")
+    const th1 = document.createElement("th")
+    const th2 = document.createElement("th")
+    const th3 = document.createElement("th")
+
+    th1.innerHTML = "Id"
+    th2.innerHTML = "Mensaje"
+    th3.innerHTML = "Fecha"
+
+    trHead.appendChild(th1)
+    trHead.appendChild(th2)
+    trHead.appendChild(th3)
+    tableHead.appendChild(trHead)
+
+    const tableBody = document.createElement("tbody")
+    for (const log of logFiltrados) {
+        const trBody = document.createElement("tr")
+        
+        const tdId = document.createElement("td")
+        tdId.innerHTML = log.id
+
+        const tdText = document.createElement("td")
+        tdText.innerHTML = log.text
+
+        const tdDate = document.createElement("td")
+        tdDate.innerHTML = log.date
+
+        trBody.appendChild(tdId)
+        trBody.appendChild(tdText)
+        trBody.appendChild(tdDate)
+
+        tableBody.appendChild(trBody)
+    }
+
+    tabla.appendChild(tableHead)
+    tabla.appendChild(tableBody)
+    divLogs.appendChild(tabla)
+}
+printarLogs(db.logs)
+
+//clearTimeot
+//clearInterval
+
+// setInterval(()=>{
+//     console.log("Hola")
+// }, 2*1000)
+
+//Generar un Interval que se ejecute cada 1500 ms, escriba "hola" por consola y se pare cuando haya dicho cuatro veces.
+// let i = 1
+// const diHola = setInterval(() => {
+//     if(i === 4){
+//         clearInterval(diHola)
+//     }
+//     else{
+//         console.log("Hola")
+//         i++
+//     }
+//     }, 1500);
+
+//Genera un Interval que cada 1/10 s desplace un 1px la tabla de logs hacia abajo
+
+let alto = 10
+const tabla = document.querySelector('.tabla-logs')
+const pxAbajo = setInterval(() => {
+    tabla.style.top = `${alto + 1}px`
+    alto++
+}, 1000/10)
--- pokedex/pokedex.js
+++ pokedex/pokedex.js
@@ -14,10 +14,10 @@ const mapPokemons = (pokemons) => {
         return pokemons.map((pokemon) => ({
         id: pokemon.id,
         name: pokemon.name,
-        image: pokemon.sprites['front_default'],
+        image: pokemon.sprites['other']['official-artwork']['front_default'],
         type: pokemon.types.map((type) => type.type.name),
         abilities: pokemon.abilities.map((ability) => ability.ability),
-        stats: pokemon.stats.map((stat) => {return {name: stat.stat.name, valor: stat.base_stat}}), //PREGUNTAR SI ESTO ES CORRECTO
+        stats: pokemon.stats.map((stat) => {return {name: stat.stat.name, valor: stat.base_stat}}),
         typeClass: pokemon.types[0].type.name   //Para darle estilo al div, según tipo principal
     }))
 }
@@ -29,7 +29,7 @@ const drawPokemons = (pokemons) => {
         const pokemonDiv$$ = document.createElement("div")
 
         pokemonDiv$$.setAttribute("id", pokemon.name)
-        pokemonDiv$$.className = `pokemon-card pokemon-card--${pokemon.typeClass}`
+        pokemonDiv$$.className = `pokemon-card pokemon-card--${pokemon.typeClass} col-5 col-md-5 col-lg-2`
         pokemonDiv$$.style.cursor = "pointer" //METER EN LA HOJA DE ESTILOS
 
         const cardHeader$$ = document.createElement("div")
@@ -40,6 +40,7 @@ const drawPokemons = (pokemons) => {
         pokemonNum$$.innerHTML = `#${pokemon.id}`
 
         const pokemonName$$ = document.createElement("h3")
+        pokemonName$$.className = "card-name"
         pokemonName$$.innerHTML = pokemon.name
 
         const cardBody$$ = document.createElement("div")
@@ -80,10 +81,21 @@ const drawPokemons = (pokemons) => {
 }
 
 const drawSearch = (pokemons) => {
-    const input$$ = document.querySelector('input')
-    input$$.addEventListener("input", () => {
-        searchPokemonsByName(input$$.value, pokemons)
+    const inputSearch$$ = document.querySelector('.input-search')
+    // const btnSearch$$ = document.querySelector('.btn-search')
+    let tempSearch = null
+    inputSearch$$.addEventListener("keyup", () => {
+        if(tempSearch){
+            clearTimeout(tempSearch)
+        }
+        tempSearch = setTimeout(() => {
+            searchPokemonsByName(inputSearch$$.value, pokemons)
+        }, 1*1000)
+        
     })
+    // inputSearch$$.addEventListener("input", () => {
+    //     searchPokemonsByName(input$$.value, pokemons)
+    // })
 }
 
 const searchPokemonsByName = (filtro, pokemons) => {
@@ -140,58 +152,117 @@ const searchPokemonFile = (filtro, pokemons) => {
 }
 
 const drawPokemonFile = async (pokemon) => {
+    console.log(pokemon);
     const abilities = await getPokemonsAbilities(pokemon[0].abilities)
-    //const filteredAbilities = filterAbilities(abilities)
+    // const filteredAbilities = filterAbilities(abilities)
     const divFicha = document.querySelector('#div-ficha')
     divFicha.innerHTML = ''
+    
     const fichaPokemon$$ = document.createElement("div")
+    fichaPokemon$$.className = "ficha-pokemon"
 
-    const pokemonName$$ = document.createElement("h4")
+    const fichaHeader$$ = document.createElement("div")
+    fichaHeader$$.className = "ficha-header"
+
+    const pokemonNum$$ = document.createElement("h3")
+    pokemonNum$$.className = "pokemon-num"
+    pokemonNum$$.innerHTML = `#${pokemon[0].id}`
+
+    const pokemonName$$ = document.createElement("h3")
+    pokemonName$$.className = "pokemon-name"
     pokemonName$$.innerHTML = pokemon[0].name
 
+    const fichaImg$$ = document.createElement("div")
+    fichaImg$$.className = "ficha-img"
+    
     const pokemonImg$$ = document.createElement("img")
     pokemonImg$$.setAttribute("src", pokemon[0].image)
     pokemonImg$$.setAttribute("alt", pokemon[0].name)
+    pokemonImg$$.className = "pokemon-img"
 
-    const pokemonNum$$ = document.createElement("span")
-    pokemonNum$$.innerHTML = pokemon[0].id
+    const fichaTypes$$ = document.createElement("div")
+    fichaTypes$$.className = "ficha-types"
 
-    const divStats$$ = document.createElement("div")
+    const pokemonTypes$$ = document.createElement("h5")
+    pokemonTypes$$.innerHTML = "Tipo"
+
+    fichaTypes$$.appendChild(pokemonTypes$$)
+
+    for (const type of pokemon[0].type) {
+        const pokemonType$$ = document.createElement("span")
+        pokemonType$$.className = "card-type"
+        pokemonType$$.innerHTML = type
+        fichaTypes$$.appendChild(pokemonType$$)
+    }      
+
+    const fichaStats$$ = document.createElement("div")
+    fichaStats$$.className = "ficha-stats"
+
+    const pokemonStats$$ = document.createElement("h5")
+    pokemonStats$$.innerHTML = "Estadísitcas"
+
+    fichaStats$$.appendChild(pokemonStats$$)
     
     for (const stat of pokemon[0].stats) {
         const divStat$$ = document.createElement("div")
-        divStat$$.setAttribute("class","") //DAR CLASE
+        divStat$$.className = "ficha-stat"
 
         const statName$$ = document.createElement("span")
+        statName$$.className = "span-stat"
         statName$$.innerHTML = stat.name
 
         const statValue$$ = document.createElement("span")
+        statValue$$.className = "span-stat span-stat--value"
         statValue$$.innerHTML = stat.valor
 
+        const divFillStat$$ = document.createElement("div")
+        divFillStat$$.className = "stat-fill"
+        if(stat.valor >= 100){
+            divFillStat$$.className = "stat-fill stat-fill--100"
+            divFillStat$$.style.width = `100%`
+        }
+        else{
+            divFillStat$$.style.width = `${stat.valor}%`
+        }        
+       
+
+        divStat$$.appendChild(divFillStat$$)
         divStat$$.appendChild(statName$$)
         divStat$$.appendChild(statValue$$)
+        
 
-        divStats$$.appendChild(divStat$$)
+        fichaStats$$.appendChild(divStat$$)
     }
-    fichaPokemon$$.appendChild(pokemonName$$)
-    fichaPokemon$$.appendChild(pokemonImg$$)
-    fichaPokemon$$.appendChild(pokemonNum$$)
-    fichaPokemon$$.appendChild(divStats$$)
+
+    const fichaAbilities$$ = document.createElement("div")
+    fichaAbilities$$.className = "ficha-abilities"
+
+    const pokemonAbilities$$ = document.createElement("h5")
+    pokemonAbilities$$.innerHTML = "Habilidades"
+
+    fichaAbilities$$.appendChild(pokemonAbilities$$)
+
+    for (const ability of pokemon[0].abilities) {
+        const pokemonAbility$$ = document.createElement("span")
+        pokemonAbility$$.className = "card-type"
+        pokemonAbility$$.innerHTML = ability.name
+        fichaAbilities$$.appendChild(pokemonAbility$$)
+    }  
+
+    fichaHeader$$.appendChild(pokemonNum$$)
+    fichaHeader$$.appendChild(pokemonName$$)
+
+    fichaImg$$.appendChild(pokemonImg$$)
+
+    fichaPokemon$$.appendChild(fichaHeader$$)
+    fichaPokemon$$.appendChild(fichaImg$$)
+    fichaPokemon$$.appendChild(fichaTypes$$)
+    fichaPokemon$$.appendChild(fichaStats$$)
+    fichaPokemon$$.appendChild(fichaAbilities$$)
 
     divFicha.appendChild(fichaPokemon$$)
 
 }
-//PREGUNTAR POR ESTA FUNCIÓN
-const filterAbilities = (abilities) => {
-    const arrayAbilities = []
-    for (const ability of abilities) {
-        for (const flavorText of ability.flavor_text_entries) {
-            if(flavorText.language.name === "es" && !arrayAbilities.includes(flavorText.flavor_text)){
-                arrayAbilities.push(flavorText.flavor_text)
-            }
-        }
-    }
-//console.log(arrayAbilities);
-    // const spanishAbilities = abilities.filter((ability) => ability.flavor_text_entries.filter((flavorText) => flavorText.language.name === "es"))
-    // console.log(spanishAbilities);
-}
+// const filterAbilities = (abilities) => {
+//     const arrayAbilities = abilities.map(x=>x.flavor_text_entries).flat().filter(x=>x.language.name === "es").map(x=>x.flavor_text)
+// }
